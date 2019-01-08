0d1fc9eb2333034f1e822f31c04fc14f7ec05931
Zacky <zacky_asyari@yahoo.com>
2019-01-08 22:45:10 +0700

    lago

---
+++ Pyhton Teaching/Belajar Python untuk pemula/untitled1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  1 14:50:32 2018
+
+@author: ASUS ROG
+"""
+
+nama_depan = 'Lionel'
+name_belakang = 'Ronaldo'
+nama_lengkap = nama_depan + " " + name_belakang
+#print(nama_lengkap)
+
+kalimat = "Hai, " + nama_lengkap + " !"
+#print(kalimat)
+
+#print("Bahasa: \nIndonesia\nInggris\nPerancis")
